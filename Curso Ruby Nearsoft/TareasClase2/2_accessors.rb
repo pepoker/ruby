ed5b509dc55eaea79dbcbe7483d7bb290a186c0b
Jose Ochoa <jochoa@tgc.mx>
2017-06-19 16:04:32 -0600

    se versionan tareas de la segunda clase

---
+++ Curso Ruby Nearsoft/TareasClase2/2_accessors.rb
@@ -0,0 +1,38 @@
+# El nombre, año y producción pueden ser leidos y modificados.
+# La fecha de creación sólo puede ser leída.
+class Movie
+	attr_accessor :name
+	attr_accessor :year
+	attr_accessor :production
+	attr_reader :created_at
+  def initialize(name, options={})
+    @name = name
+    @year = options[:year]
+    @production = options[:production]
+    @created_at = Time.now
+  end
+
+  def build_message
+    "La pelicula #{name} fue estrenada en #{year} por #{production}"
+  end
+end
+
+movie_1 = Movie.new("Thor", options = {year: 2017, production: 'Disney'})
+movie_1.year = 2011
+movie_1.production = 'Marvel Studios'
+
+# Resultado esperado:
+# La película Thor fue estrenada en 2011 por Marvel Studios.
+puts movie_1.build_message # Solución
+
+
+movie_2 = Movie.new("Inception", options = {year: 2010, production: 'WB'})
+
+# Descomenta la línea con created_at, cuando corras la aplicación debe fallar.
+# Después de verificar que falla, coméntala de nuevo.
+
+# movie_2.created_at = '2016-06-02'
+
+# Resultado esperado:
+# La película Inception fue estrenada en 2010 por WB.
+puts movie_2.build_message # Solución
---
+++ Curso Ruby Nearsoft/TareasClase2/arrays.rb
@@ -0,0 +1,15 @@
+puts "Crea un arreglo e imprime, después agrega un elemento y vuelve a imprimir,
+      despues imprime el ultimo elemento de tu arreglo."
+
+# Declara tu primer arreglo aqui
+arr= ["primero", "dos"]
+puts "Arreglo original"
+puts arr
+
+# Agrega un dato a tu arreglo
+arr << "Otroo"
+puts "Arreglo con nuevo elemento"
+puts arr
+
+puts "Ultimo elemento del arreglo"
+puts arr[-1]
